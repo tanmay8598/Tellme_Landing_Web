7bcf5d43afdf9f909073bff99a4c0b745dfce667
Ashish Pal <ashishpal0502@gmail.com>
2025-05-22 10:00:00 +0530

    footer and site title changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,9 +13,7 @@ function Footer() {
         <h2 className="text-2xl text-white font-bold mb-4">
           Click. Connect. Breathe.
         </h2>
-        <p className="mb-4 text-white ">
-          Your first 10 minutes are free to feel heard.
-        </p>
+
         <motion.button
           className="bg-[#FFF4EC] text-[#352F2F] px-6 py-3 rounded-full shadow-md hover:opacity-90 transition"
           whileTap={{ scale: 0.95 }}
